f25bf2f09e3d30dbb4192e9a7d4510e412f7ca6d
Arly Candia <arly.candia@gmail.com>
2026-02-13 21:58:40 -0300

    mostrar productos de una lista by Json

--- 02_leer_api/js/main.js
+++ 02_leer_api/js/main.js
@@ -1,3 +1,14 @@
 const resultado = document.getElementById("resultado");
 
-document.getElementById("boton").addEventListener("click", () => {});
+document.getElementById("boton").addEventListener("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((result) => result.json())
+    .then((data) => getData(data))
+    .catch((error) => console.log(err));
+});
+
+function getData(data) {
+  data.forEach((item) => {
+    resultado.innerHTML += `<p>${item.name}, - ${item.email}</p>`;
+  });
+}
